7db8291dab3d190bbd452ac520bbc509f80d1a1d
Borgbyte <borgbyte@gmail.com>
2019-03-19 10:58:42 +0000

    Add application skeleton

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickStyle>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setApplicationName("Green");
+    QCoreApplication::setOrganizationName("Blockstream");
+    QCoreApplication::setOrganizationDomain("blockstream.com");
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
+    QQuickStyle::setStyle("Material");
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
---
+++ src/main.qml
@@ -0,0 +1,13 @@
+import QtQuick 2.12
+import QtQuick.Window 2.12
+import QtQuick.Controls 2.5
+import QtQuick.Controls.Material 2.3
+
+ApplicationWindow {
+    visible: true
+    width: 640
+    height: 480
+    title: 'Blockstream Green'
+
+    Material.theme: Material.Dark
+}
