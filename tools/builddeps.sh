99a3df288aec11de64013acd68747141984e96f3
Lawrence Nahum <lawrence@greenaddress.it>
2019-09-14 00:22:03 +0200

    make sure things are rebuild if deps change

--- tools/builddeps.sh
+++ tools/builddeps.sh
@@ -3,11 +3,6 @@ set -eo pipefail
 
 export GREENPLATFORM=$1
 
-export GDKBLDID=$(echo $(cd gdk && git rev-parse HEAD) $(shasum -a 256 ./tools/buildgdk.sh | cut -d" " -f1) | shasum -a 256 | cut -d" " -f1)
-export QTMAJOR=5.13
-export QTVERSION=${QTMAJOR}.1
-export QTBLDID=$(echo ${QTVERSION} $(shasum -a 256 ./tools/buildqt.sh | cut -d" " -f1) | shasum -a 256 | cut -d" " -f1)
-
 if [ "${GREENPLATFORM}" = "" ]; then
     BUILDDIR=build-linux-gcc
     GREENPLATFORM="linux"
@@ -20,6 +15,13 @@ elif [ "${GREENPLATFORM}" = "osx" ]; then
 else
     exit 1
 fi
+
+DEPS=$(shasum -a 256 ./tools/bionic_deps.sh | cut -d" " -f1)
+DEPS="$DEPS $(shasum -a 256 Dockerfile | cut -d" " -f1)"
+export GDKBLDID=$(echo $(cd gdk && git rev-parse HEAD) $(shasum -a 256 ./tools/buildgdk.sh | cut -d" " -f1) ${DEPS} | shasum -a 256 | cut -d" " -f1)
+export QTMAJOR=5.13
+export QTVERSION=${QTMAJOR}.1
+export QTBLDID=$(echo ${QTVERSION} ${DEPS} $(shasum -a 256 ./tools/buildqt.sh | cut -d" " -f1) | shasum -a 256 | cut -d" " -f1)
 
 mkdir -p ${BUILDDIR}
 
