6f0cd982bddf1dedc28e3ae7feab1c0d241fad08
Borgbyte <borgbyte@gmail.com>
2020-10-27 14:38:17 +0000

    Add parsing/conversion utility functions

--- src/util.cpp
+++ src/util.cpp
@@ -16,3 +16,37 @@ QString GetDataFile(const QString& context, const QString& name)
 {
     return GetDataDir(context) + QDir::separator() + name;
 }
+
+quint64 ParseSatoshi(const QJsonValue &value)
+{
+    return value.toDouble();
+}
+
+QVector<uint32_t> ParsePath(const QJsonValue &value)
+{
+    return ParsePath(value.toArray());
+}
+
+QVector<uint32_t> ParsePath(const QJsonArray &array)
+{
+    QVector<uint32_t> path;
+    for (const auto value : array) {
+        path.append(value.toDouble());
+    }
+    return path;
+}
+
+quint32 ParseLocktime(const QJsonValue &value)
+{
+    return value.toDouble();
+}
+
+quint32 ParseSequence(const QJsonValue &value)
+{
+    return value.toDouble();
+}
+
+QByteArray ParseByteArray(const QJsonValue &value)
+{
+    return QByteArray::fromHex(value.toString().toLocal8Bit());
+}
--- src/util.h
+++ src/util.h
@@ -1,9 +1,20 @@
 #ifndef GREEN_UTIL_H
 #define GREEN_UTIL_H
 
+#include <QByteArray>
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QJsonArray>
 #include <QString>
 
 QString GetDataDir(const QString& context);
 QString GetDataFile(const QString& context, const QString& name);
+
+QVector<uint32_t> ParsePath(const QJsonArray& array);
+QVector<uint32_t> ParsePath(const QJsonValue& value);
+quint32 ParseLocktime(const QJsonValue& value);
+quint64 ParseSatoshi(const QJsonValue& value);
+quint32 ParseSequence(const QJsonValue& value);
+QByteArray ParseByteArray(const QJsonValue& value);
 
 #endif // GREEN_UTIL_H
