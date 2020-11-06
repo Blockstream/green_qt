79c975d3a38e9fba000738eef38c8194612b453d
Borgbyte <borgbyte@gmail.com>
2020-11-06 16:11:06 +0000

    Remove unused GA namespace functions

--- src/ga.cpp
+++ src/ga.cpp
@@ -6,32 +6,6 @@
 
 namespace GA {
 
-int reconnect_hint(GA_session* session, const QJsonObject& data)
-{
-    GA_json* hint = Json::fromObject(data);
-    int err = GA_reconnect_hint(session, hint);
-    GA_destroy_json(hint);
-    return err;
-}
-
-int connect(GA_session* session, const QJsonObject& data)
-{
-    GA_json* net_params = Json::fromObject(data);
-    int err = GA_connect(session, net_params);
-    GA_destroy_json(net_params);
-    return err;
-}
-
-QJsonObject auth_handler_get_result(GA_auth_handler* call)
-{
-    GA_json* output;
-    int err = GA_auth_handler_get_status(call, &output);
-    Q_ASSERT(err == GA_OK);
-    auto result = Json::toObject(output);
-    GA_destroy_json(output);
-    return result;
-}
-
 QJsonObject convert_amount(GA_session* session, const QJsonObject& input)
 {
     GA_json* value_details = Json::fromObject(input);
--- src/ga.h
+++ src/ga.h
@@ -5,13 +5,9 @@
 #include <QJsonObject>
 
 struct GA_session;
-struct GA_auth_handler;
 
 namespace GA {
 
-int reconnect_hint(GA_session* session, const QJsonObject& data);
-int connect(GA_session* session, const QJsonObject& data);
-QJsonObject auth_handler_get_result(GA_auth_handler* call);
 QJsonObject convert_amount(GA_session* session, const QJsonObject& input);
 QStringList generate_mnemonic();
 
