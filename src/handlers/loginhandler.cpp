7d23f84aa7c3fabd62c969a8063819b362883bf1
Borgbyte <borgbyte@gmail.com>
2020-11-06 12:42:12 +0000

    Refactor LoginHandler constructor to support device

--- src/handlers/loginhandler.cpp
+++ src/handlers/loginhandler.cpp
@@ -1,24 +1,37 @@
+#include "json.h"
 #include "loginhandler.h"
 
 #include <gdk.h>
 
-LoginHandler::LoginHandler(Wallet *wallet, const QStringList &mnemonic, const QString &password)
+LoginHandler::LoginHandler(Wallet* wallet, const QStringList &mnemonic)
+    : Handler(wallet)
+    , m_mnemonic(mnemonic)
+{
+    Q_ASSERT(m_mnemonic.size() == 24);
+}
+
+LoginHandler::LoginHandler(Wallet* wallet, const QStringList& mnemonic, const QString& password)
     : Handler(wallet)
     , m_mnemonic(mnemonic)
     , m_password(password)
 {
-    Q_ASSERT(m_mnemonic.size() == 24 || (m_mnemonic.size() == 27 && !m_password.isEmpty()));
+    Q_ASSERT(m_mnemonic.size() == 27 && !m_password.isEmpty());
+}
+
+LoginHandler::LoginHandler(Wallet* wallet, const QJsonObject& device_details)
+    : Handler(wallet)
+    , m_device_details(device_details)
+{
+    // TODO: assert device_details or use Device instance to infer them
 }
 
 void LoginHandler::call(GA_session *session, GA_auth_handler **auth_handler)
 {
     QByteArray mnemonic = m_mnemonic.join(' ').toLocal8Bit();
     QByteArray password = m_password.toLocal8Bit();
-    GA_json* device;
-    int err = GA_convert_string_to_json("{}", &device);
+    GA_json* device_details = Json::fromObject(m_device_details);
+    int err = GA_login(session, device_details, mnemonic.constData(), password.constData(), auth_handler);
     Q_ASSERT(err == GA_OK);
-    err = GA_login(session, device, mnemonic.constData(), password.constData(), auth_handler);
-    Q_ASSERT(err == GA_OK);
-    err = GA_destroy_json(device);
+    err = GA_destroy_json(device_details);
     Q_ASSERT(err == GA_OK);
 }
--- src/handlers/loginhandler.h
+++ src/handlers/loginhandler.h
@@ -9,10 +9,13 @@ class LoginHandler : public Handler
 {
     Q_OBJECT
 public:
-    LoginHandler(Wallet* wallet, const QStringList& mnemonic, const QString& password = {});
+    LoginHandler(Wallet* wallet, const QStringList& mnemonic);
+    LoginHandler(Wallet* wallet, const QStringList& mnemonic, const QString& password);
+    LoginHandler(Wallet* wallet, const QJsonObject& device_details);
 private:
     const QStringList m_mnemonic;
     const QString m_password;
+    const QJsonObject m_device_details;
     void call(GA_session* session, GA_auth_handler** auth_handler) override;
 };
 
