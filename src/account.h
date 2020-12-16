6c54c01cc5f88f9ad290bf9b74ea48d2f23e011a
Borgbyte <borgbyte@gmail.com>
2020-12-16 12:34:24 +0000

    Move ReceiveAddressController to dedicated file

--- src/account.h
+++ src/account.h
@@ -61,47 +61,4 @@ public:
     Transaction *getOrCreateTransaction(const QJsonObject &data);
 };
 
-QML_DECLARE_TYPE(Account*);
-
-class ReceiveAddressController : public QObject
-{
-    Q_OBJECT
-    Q_PROPERTY(Account* account READ account WRITE setAccount NOTIFY accountChanged)
-    Q_PROPERTY(QString amount READ amount WRITE setAmount NOTIFY changed)
-    Q_PROPERTY(QString address READ address NOTIFY changed)
-    Q_PROPERTY(QString uri READ uri NOTIFY changed)
-    Q_PROPERTY(bool generating READ generating NOTIFY generatingChanged)
-    QML_ELEMENT
-public:
-    explicit ReceiveAddressController(QObject* parent = nullptr);
-    virtual ~ReceiveAddressController();
-
-    Account* account() const;
-    void setAccount(Account* account);
-
-    QString amount() const;
-    void setAmount(const QString& amount);
-
-    QString address() const;
-
-    QString uri() const;
-
-    bool generating() const;
-    void setGenerating(bool generating);
-
-public slots:
-    void generate();
-
-signals:
-    void accountChanged(Account* account);
-    void changed();
-    void generatingChanged(bool generating);
-
-public:
-    Account* m_account{nullptr};
-    QString m_amount;
-    QString m_address;
-    bool m_generating{false};
-};
-
 #endif // GREEN_ACCOUNT_H
--- src/controllers/controllers.pri
+++ src/controllers/controllers.pri
@@ -3,9 +3,11 @@ INCLUDEPATH += $$PWD
 HEADERS += \
     $$PWD/exporttransactionscontroller.h \
     $$PWD/ledgerdevicecontroller.h \
+    $$PWD/receiveaddresscontroller.h \
     $$PWD/systemmessagecontroller.h
 
 SOURCES += \
     $$PWD/exporttransactionscontroller.cpp \
     $$PWD/ledgerdevicecontroller.cpp \
+    $$PWD/receiveaddresscontroller.cpp \
     $$PWD/systemmessagecontroller.cpp
