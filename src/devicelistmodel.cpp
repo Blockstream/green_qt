fbf71b9e894fea220c6f6b938569b8dad72e5835
Borgbyte <borgbyte@gmail.com>
2021-01-30 17:49:27 +0000

    Allow to filter by device vendor and type

--- src/devicelistmodel.cpp
+++ src/devicelistmodel.cpp
@@ -21,6 +21,22 @@ DeviceListModel::DeviceListModel(QObject* parent)
     sort(0);
 }
 
+void DeviceListModel::setVendor(Device::Vendor vendor)
+{
+    if (m_vendor == vendor) return;
+    m_vendor = vendor;
+    emit vendorChanged(m_vendor);
+    invalidateFilter();
+}
+
+void DeviceListModel::setType(Device::Type type)
+{
+    if (m_type == type) return;
+    m_type = type;
+    emit typeChanged(m_type);
+    invalidateFilter();
+}
+
 void DeviceListModel::addDevice(Device* device)
 {
     auto item = new QStandardItem;
@@ -34,4 +50,12 @@ void DeviceListModel::removeDevice(Device* device)
     auto item = m_items.take(device);
     m_source_model.takeRow(item->row());
     delete item;
+}
+
+bool DeviceListModel::filterAcceptsRow(int source_row, const QModelIndex &source_parent) const
+{
+    auto device = m_source_model.index(source_row, 0, source_parent).data(Qt::UserRole).value<Device*>();
+    if (m_vendor != Device::NoVendor && device->vendor() != m_vendor) return false;
+    if (m_type != Device::NoType && device->type() != m_type) return false;
+    return true;
 }
--- src/devicelistmodel.h
+++ src/devicelistmodel.h
@@ -7,23 +7,35 @@
 #include <QStandardItem>
 #include <QStandardItemModel>
 
-class Device;
+#include "device.h"
 
 class DeviceListModel : public QSortFilterProxyModel
 {
     Q_OBJECT
+    Q_PROPERTY(Device::Vendor vendor READ vendor WRITE setVendor NOTIFY vendorChanged)
+    Q_PROPERTY(Device::Type type READ type WRITE setType NOTIFY typeChanged)
     Q_PROPERTY(int rowCount READ rowCount NOTIFY rowCountChanged)
     QML_ELEMENT
 public:
     DeviceListModel(QObject* parent = nullptr);
+    Device::Vendor vendor() const { return m_vendor; }
+    void setVendor(Device::Vendor vendor);
+    Device::Type type() const { return m_type; }
+    void setType(Device::Type type);
 private slots:
     void addDevice(Device* device);
     void removeDevice(Device* device);
 signals:
     void rowCountChanged();
+    void vendorChanged(Device::Vendor vendor);
+    void typeChanged(Device::Type type);
+protected:
+    bool filterAcceptsRow(int source_row, const QModelIndex& source_parent) const override;
 private:
     QStandardItemModel m_source_model;
     QMap<Device*, QStandardItem*> m_items;
+    Device::Vendor m_vendor{Device::NoVendor};
+    Device::Type m_type{Device::NoType};
 };
 
 #endif // GREEN_DEVICELISTMODEL_H
