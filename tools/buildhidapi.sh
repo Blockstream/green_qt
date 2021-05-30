f8f759d50f01cf08b4d4722caafbb5581fc76b1a
Borgbyte <borgbyte@gmail.com>
2021-05-30 22:54:40 +0100

    Fix hidapi build due to duplicate AC_CONFIG_MACRO_DIR

--- tools/buildhidapi.sh
+++ tools/buildhidapi.sh
@@ -15,6 +15,11 @@ fi
 
 cd ${HIDAPI_PATH}/src
 
+# Remove duplicate AC_CONFIG_MACRO_DIR
+# see https://github.com/libusb/hidapi/commit/d15d594a415e5f51d2cd8ff8bb77b3765a539b98
+# (can be removed when hidapi is updated)
+sed -i.bak '16d' configure.ac
+
 ./bootstrap
 if [ "$GREENPLATFORM" = "linux" ]; then
     PKG_CONFIG_PATH=${LIBUSB_PATH}/lib/pkgconfig ./configure --prefix=${HIDAPI_PATH} >> ${HIDAPI_PATH}/build.log 2>&1
