fe74d8a42a5101d5842785f3a31a4eeee78b5e7d
Borgbyte <borgbyte@gmail.com>
2021-08-16 16:24:06 +0100

    Set no-store cache control header for latest.json

--- tools/publish.sh
+++ tools/publish.sh
@@ -32,7 +32,7 @@ sed -i -e "s/HASH_LINUX/${HASH_LINUX}/g" ${CHANNEL}.json
 
 echo $GCLOUD_KEY > .key
 gcloud auth activate-service-account --key-file=.key
-gsutil cp ${CHANNEL}.json gs://${GCLOUD_BUCKET}/desktop/
+gsutil cp -h "Cache-Control: no-store" ${CHANNEL}.json gs://${GCLOUD_BUCKET}/desktop/
 
 gsutil cp SHA256SUMS.asc gs://${GCLOUD_BUCKET}/desktop/${VERSION}/
 gsutil cp BlockstreamGreen_Windows_x86_64.zip gs://${GCLOUD_BUCKET}/desktop/${VERSION}/
