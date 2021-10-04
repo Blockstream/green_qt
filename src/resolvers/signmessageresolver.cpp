744012e427fbbe9cb4fe8d8e33f8cac999571f0c
Borgbyte <borgbyte@gmail.com>
2021-10-04 10:51:34 +0100

    Hash message in sign message resolver

--- src/resolvers/signmessageresolver.cpp
+++ src/resolvers/signmessageresolver.cpp
@@ -3,9 +3,31 @@
 #include "signmessageresolver.h"
 #include "util.h"
 
+#include <wally_crypto.h>
+
+namespace {
+
+QByteArray HashMessage(const QString& message)
+{
+    QByteArray data = message.toUtf8();
+    QByteArray hash(SHA256_LEN, 0);
+    size_t written;
+    auto rc = wally_format_bitcoin_message(
+        (unsigned char*) data.constData(), data.size(),
+        BITCOIN_MESSAGE_FLAG_HASH,
+        (unsigned char*) hash.data(), hash.size(),
+        &written);
+    Q_ASSERT(rc == WALLY_OK);
+    Q_ASSERT(written == SHA256_LEN);
+    return hash.toHex();
+}
+
+}
+
 SignMessageResolver::SignMessageResolver(Handler* handler, const QJsonObject& result)
     : DeviceResolver(handler, result)
     , m_message(m_required_data.value("message").toString())
+    , m_hash(HashMessage(m_message))
     , m_path(ParsePath(m_required_data.value("path").toArray()))
     , m_use_ae_protocol(m_required_data.value("use_ae_protocol").toBool())
     , m_ae_host_commitment(ParseByteArray(m_required_data.value("ae_host_commitment")))
--- src/resolvers/signmessageresolver.h
+++ src/resolvers/signmessageresolver.h
@@ -7,15 +7,18 @@ class SignMessageResolver : public DeviceResolver
 {
     Q_OBJECT
     Q_PROPERTY(QString message READ message CONSTANT)
+    Q_PROPERTY(QString hash READ hash CONSTANT)
     Q_PROPERTY(QString path READ path CONSTANT)
     QML_ELEMENT
 public:
     SignMessageResolver(Handler* handler, const QJsonObject& result);
     QString message() const { return m_message; }
+    QString hash() const { return m_hash; }
     QString path() const;
     void resolve() override;
 private:
     const QString m_message;
+    const QString m_hash;
     const QVector<uint32_t> m_path;
     const bool m_use_ae_protocol;
     const QByteArray m_ae_host_commitment;
