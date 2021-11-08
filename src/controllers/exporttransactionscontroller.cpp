4bd977de8d920bf576167e15dc5f49b834073a7c
Borgbyte <borgbyte@gmail.com>
2021-11-08 15:27:54 +0000

    Use wallet name in export transactions controller

--- src/controllers/exporttransactionscontroller.cpp
+++ src/controllers/exporttransactionscontroller.cpp
@@ -33,11 +33,10 @@ void ExportTransactionsController::save()
     auto settings = wallet->settings();
 
     const auto now = QDateTime::currentDateTime();
-    const auto name = wallet->device() ? wallet->device()->name() : wallet->name();
     const auto account_name = m_account->name().isEmpty() ? qtTrId("id_main_account") : m_account->name();
     const QString suggestion =
             QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation) + QDir::separator() +
-            name  + " - " + account_name + " - " +
+            wallet->name() + " - " + account_name + " - " +
             now.toString("yyyyMMddhhmmss") + ".csv";
     m_file_name = QFileDialog::getSaveFileName(nullptr, "Export to CSV", suggestion);
     if (m_file_name.isEmpty()) {
