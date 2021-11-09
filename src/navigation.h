bec524a6a21fbbd2e8127af11c78cc0c4c596519
Borgbyte <borgbyte@gmail.com>
2021-11-09 15:23:53 +0000

    Remove unused Route

--- src/navigation.h
+++ src/navigation.h
@@ -36,37 +36,4 @@ private:
     QVariantMap m_param;
 };
 
-class Route : public QObject
-{
-    Q_OBJECT
-    Q_PROPERTY(QString location READ location WRITE setLocation NOTIFY locationChanged)
-    Q_PROPERTY(QString previous READ previous NOTIFY previousChanged)
-    Q_PROPERTY(QString path READ path WRITE setPath NOTIFY pathChanged)
-    Q_PROPERTY(bool active READ isActive NOTIFY activeChanged)
-    QML_ELEMENT
-public:
-    Route(QObject* parent = nullptr);
-    QString location() const { return m_location; }
-    void setLocation(const QString& location);
-    QString previous() const { return m_previous ; }
-    QString path() const { return m_path; }
-    void setPath(const QString& path);
-    bool isActive() const { return m_active; }
-public slots:
-signals:
-    void locationChanged(const QString& location);
-    void previousChanged(const QString& previous);
-    void pathChanged(const QString& path);
-    void activeChanged(bool active);
-private:
-    void update();
-    void setPrevious(const QString& previous);
-    void setActive(bool active);
-private:
-    QString m_location;
-    QString m_previous;
-    QString m_path;
-    bool m_active{false};
-};
-
 #endif // GREEN_NAVIGATION_H
