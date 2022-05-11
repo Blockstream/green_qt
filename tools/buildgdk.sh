0a9ab9ef80668c1e4125ec0e004e12a43af16f9d
Borgbyte <borgbyte@gmail.com>
2022-05-11 17:28:22 +0200

    gdk: rust is mandatory

--- tools/buildgdk.sh
+++ tools/buildgdk.sh
@@ -29,12 +29,12 @@ fi
 
 source ${GDKVENV}/bin/activate >> ${GDK_PATH}/build.log 2>&1
 if [ "$GREENPLATFORM" = "linux" ]; then
-    tools/build.sh --gcc --lto=true --enable-rust >> ${GDK_PATH}/build.log 2>&1
+    tools/build.sh --gcc --lto=true >> ${GDK_PATH}/build.log 2>&1
 elif [ "$GREENPLATFORM" = "windows" ]; then
-    tools/build.sh --mingw-w64 --lto=false --enable-rust >> ${GDK_PATH}/build.log 2>&1
+    tools/build.sh --mingw-w64 --lto=false >> ${GDK_PATH}/build.log 2>&1
 elif [ "$GREENPLATFORM" = "osx" ]; then
     unset JAVA_HOME
-    tools/build.sh --clang --enable-rust >> ${GDK_PATH}/build.log 2>&1
+    tools/build.sh --clang >> ${GDK_PATH}/build.log 2>&1
 else
     exit 1
 fi
