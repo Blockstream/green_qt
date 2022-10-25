c7f4bad76d5708043338a32210a1de31389ae754
borgbyte <borgbyte@gmail.com>
2022-10-25 13:34:24 +0100

    singlesig: dont create segwit subaccount

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -15,7 +15,6 @@
 #include "account.h"
 #include "asset.h"
 #include "balance.h"
-#include "createaccounthandler.h"
 #include "device.h"
 #include "ga.h"
 #include "handler.h"
@@ -228,28 +227,13 @@ void Wallet::reload(bool refresh_accounts)
     auto handler = new GetSubAccountsHandler(m_session, refresh_accounts);
     QObject::connect(handler, &Handler::done, this, [this, handler] {
         handler->deleteLater();
-        bool has_segwit = false;
 
         m_accounts.clear();
         for (auto value : handler->subAccounts()) {
             QJsonObject data = value.toObject();
             Account* account = getOrCreateAccount(data);
             account->reload();
-            has_segwit |= account->type() == "p2wpkh";
             m_accounts.append(account);
-        }
-
-        if (m_network->isElectrum() && !has_segwit) {
-            auto handler = new CreateAccountHandler({{ "name", "Segwit Account" }, { "type", "p2wpkh" }}, m_session);
-            QObject::connect(handler, &Handler::done, this, [=] {
-                handler->deleteLater();
-                reload(false);
-            });
-            QObject::connect(handler, &Handler::resolver, this, [](Resolver* resolver) {
-                resolver->resolve();
-            });
-            handler->exec();
-            return;
         }
 
         emit accountsChanged();
