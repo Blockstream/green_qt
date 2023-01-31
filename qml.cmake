94b3ac5a583dc46218c1e7d1136e857a6668ce4a
Borgbyte <borgbyte@gmail.com>
2023-01-31 17:11:54 +0100

    [skip ci] cmake: initial version

---
+++ qml.cmake
@@ -0,0 +1,182 @@
+qt_add_resources(xgreen "qtquickcontrols2"
+    PREFIX "/"
+    BASE src
+    FILES
+        src/qtquickcontrols2.conf
+)
+
+SET(QML_FILES
+    qml/analytics.js
+    qml/util.js
+    qml/SelectServerTypeView.qml
+    qml/WalletSecuritySettingsView.qml
+    qml/GCard.qml
+    qml/TwoFactorEnableDialog.qml
+    qml/MnemonicDialog.qml
+    qml/LedgerDevicesView.qml
+    qml/DebugRectangle.qml
+    qml/WalletGeneralSettingsView.qml
+    qml/NetworkView.qml
+    qml/TransactionView.qml
+    qml/HSpacer.qml
+    qml/WalletDialog.qml
+    qml/StackViewPushAction.qml
+    qml/SignLiquidTransactionResolverView.qml
+    qml/GHeader.qml
+    qml/Wallet2faSettingsView.qml
+    qml/BlockstreamView.qml
+    qml/SessionBadge.qml
+    qml/GComboBox.qml
+    qml/TwoFactorAuthExpiryDialog.qml
+    qml/Tag.qml
+    qml/JadeViewDeviceNetwork.qml
+    qml/AccountDelegate.qml
+    qml/DeleteWalletDialog.qml
+    qml/ChangePinDialog.qml
+    qml/WalletSideButton.qml
+    qml/MnemonicView.qml
+    qml/TwoFactorLimitDialog.qml
+    qml/JadeUpdateDialog.qml
+    qml/JadeSignMessageView.qml
+    qml/MainMenuBar.qml
+    qml/GTextField.qml
+    qml/NLockTimeDialog.qml
+    qml/MessageDialog.qml
+    qml/SignupDialog.qml
+    qml/ConnectingView.qml
+    qml/SelectNetworkViewCard.qml
+    qml/GSwitch.qml
+    qml/ReceiveView.qml
+    qml/SetUnspentOutputsStatusDialog.qml
+    qml/SendView.qml
+    qml/AccountListView.qml
+    qml/VSpacer.qml
+    qml/WizardButtonBox.qml
+    qml/SelectServerTypeViewCard.qml
+    qml/AbstractDialog.qml
+    qml/ControllerDialog.qml
+    qml/CreateAccountDialog.qml
+    qml/WizardPage.qml
+    qml/Spacer.qml
+    qml/WalletListDelegate.qml
+    qml/AssetDelegate.qml
+    qml/RemoveWalletDialog.qml
+    qml/DialogFooter.qml
+    qml/OutputDelegate.qml
+    qml/MnemonicPage.qml
+    qml/PersistentLoader.qml
+    qml/AnimLoader.qml
+    qml/GToolButton.qml
+    qml/AccountTypeBadge.qml
+    qml/WalletViewFooter.qml
+    qml/DiscoverServerTypeView.qml
+    qml/LoginWithPasswordView.qml
+    qml/PreferencesView.qml
+    qml/AssetView.qml
+    qml/AddressesListView.qml
+    qml/LoginWithPinView.qml
+    qml/PinField.qml
+    qml/AssetListView.qml
+    qml/DescriptiveRadioButton.qml
+    qml/SettingsPage.qml
+    qml/OverviewView.qml
+    qml/CopyableLabel.qml
+    qml/TransactionProgress.qml
+    qml/BumpFeeDialog.qml
+    qml/QRCode.qml
+    qml/JadeSignLiquidTransactionView.qml
+    qml/GButton.qml
+    qml/ReceiveDialog.qml
+    qml/WordField.qml
+    qml/WalletViewHeader.qml
+    qml/GSearchField.qml
+    qml/SystemMessageDialog.qml
+    qml/AmountValidator.qml
+    qml/DisableAllPinsDialog.qml
+    qml/JadeViewDevice.qml
+    qml/MnemonicQuizPage.qml
+    qml/OutputsListView.qml
+    qml/LeftArrowToolTip.qml
+    qml/GPane.qml
+    qml/main.qml
+    qml/DialogHeader.qml
+    qml/WalletSettingsDialog.qml
+    qml/TransactionListView.qml
+    qml/CancelTwoFactorResetDialog.qml
+    qml/GTextArea.qml
+    qml/Collapsible.qml
+    qml/ScannerPopup.qml
+    qml/DiscoverServerTypeViewCard.qml
+    qml/DeviceImage.qml
+    qml/ReviewView.qml
+    qml/LoginDialog.qml
+    qml/DeviceBadge.qml
+    qml/SelectCoinsView.qml
+    qml/AccountIdBadge.qml
+    qml/AccountView.qml
+    qml/TwoFactorDisableDialog.qml
+    qml/TransactionDelegate.qml
+    qml/AnalyticsConsentDialog.qml
+    qml/BalanceItem.qml
+    qml/JadeSignTransactionView.qml
+    qml/SideBar.qml
+    qml/AlertView.qml
+    qml/WalletAdvancedSettingsView.qml
+    qml/RequestTwoFactorResetDialog.qml
+    qml/BlinkAnimation.qml
+    qml/SendDialog.qml
+    qml/FeeComboBox.qml
+    qml/EditableLabel.qml
+    qml/FixedErrorBadge.qml
+    qml/QRCodePopup.qml
+    qml/GListView.qml
+    qml/ProgressIndicator.qml
+    qml/ScannerView.qml
+    qml/StatusBar.qml
+    qml/PinView.qml
+    qml/GFlickable.qml
+    qml/DeviceView.qml
+    qml/TransactionStatusBadge.qml
+    qml/PopupBalloon.qml
+    qml/SectionLabel.qml
+    qml/AddressDelegate.qml
+    qml/Constants.qml
+    qml/TransactionDoneView.qml
+    qml/JadeView.qml
+    qml/SideButton.qml
+    qml/SettingsBox.qml
+    qml/WalletRecoverySettingsView.qml
+    qml/CoinDelegate.qml
+    qml/MainPage.qml
+    qml/WalletView.qml
+    qml/SetRecoveryEmailDialog.qml
+    qml/MainPageHeader.qml
+    qml/MnemonicEditor.qml
+    qml/LedgerSignTransactionView.qml
+    qml/SignTransactionResolverView.qml
+    qml/RestoreDialog.qml
+    qml/AssetIcon.qml
+    qml/SelectNetworkView.qml
+    qml/WelcomePage.qml
+    qml/WatchOnlyLoginDialog.qml
+    qml/HomeView.qml
+)
+
+if (GREEN_NO_RESOURCES)
+    set(QML_FILES_2)
+    target_compile_definitions(xgreen PRIVATE SOURCE_DIR=${CMAKE_SOURCE_DIR})
+else()
+    set(QML_FILES_2 ${QML_FILES})
+endif()
+
+qt_add_qml_module(xgreen
+    URI Blockstream.Green
+    VERSION 1.0
+    NO_PLUGIN
+    DEPENDENCIES QtQuick
+    SOURCES src/networkmanager.cpp
+    QML_FILES ${QML_FILES_2}
+    #ENABLE_TYPE_COMPILER
+    NO_CACHEGEN
+    NO_LINT
+)
---
+++ translations.cmake
@@ -0,0 +1,20 @@
+qt_add_translations(xgreen
+    TS_FILES
+        i18n/green_cs.ts
+        i18n/green_de.ts
+        i18n/green_en.ts
+        i18n/green_es.ts
+        i18n/green_fr.ts
+        i18n/green_he.ts
+        i18n/green_it.ts
+        i18n/green_ja.ts
+        i18n/green_ko.ts
+        i18n/green_nl.ts
+        i18n/green_pt_BR.ts
+        i18n/green_pt_PT.ts
+        i18n/green_ro.ts
+        i18n/green_ru.ts
+        i18n/green_uk.ts
+        i18n/green_vi.ts
+        i18n/green_zh.ts
+)
