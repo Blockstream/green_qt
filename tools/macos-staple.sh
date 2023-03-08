5107b2a7e135463857ae1ed2daf692fdb402906c
Borgbyte <borgbyte@gmail.com>
2023-03-08 15:18:02 +0000

    build: print macos submission log

--- tools/macos-staple.sh
+++ tools/macos-staple.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 set -eox pipefail
 
-file=green.app
-zip=green.zip
+FILE=green.app
 TEAM_ID=D9W37S9468
 
 codesign \
   --options runtime \
   --entitlements entitlements.plist \
-  --deep $file \
+  --deep $FILE \
   -s "Developer ID Application: Blockstream Corporation ($TEAM_ID)"
 
-ditto -c -k --keepParent $file $zip
+ditto -c -k --keepParent $FILE $FILE.zip
 
-xcrun notarytool submit -v \
+xcrun notarytool submit \
+  --verbose \
+  --output-format json \
   --team-id "$TEAM_ID" \
   --apple-id "${STAPLEEMAIL}" \
   --password "${STAPLEPW}" \
   --wait \
-  $zip
+  $FILE.zip | tee submission.json
 
-xcrun stapler staple $file
+xcrun notarytool log \
+  --team-id "$TEAM_ID" \
+  --apple-id "${STAPLEEMAIL}" \
+  --password "${STAPLEPW}" \
+  $(jq -r .id submission.json)
+
+xcrun stapler staple $FILE
