36f81f8524e8a3b2a89803735cb1552060e32713
Borgbyte <borgbyte@gmail.com>
2023-05-02 22:18:15 +0100

    gdk: update to 0.0.62

--- tools/buildgdk.sh
+++ tools/buildgdk.sh
@@ -6,7 +6,7 @@ mkdir -p build
 cd build
 
 if [ ! -d gdk ]; then
-    git clone --quiet --depth 1 --branch release_0.0.61 --single-branch https://github.com/Blockstream/gdk.git gdk
+    git clone --quiet --depth 1 --branch release_0.0.62 --single-branch https://github.com/Blockstream/gdk.git gdk
 fi
 
 cd gdk
