1a293ee161973d0bef1e1f59d08cbc9ee118d982
Borgbyte <borgbyte@gmail.com>
2023-06-05 22:34:57 +0100

    jade: scan serial ports in background thread

--- src/jade/jadedeviceserialportdiscoveryagent.cpp
+++ src/jade/jadedeviceserialportdiscoveryagent.cpp
@@ -2,6 +2,7 @@
 
 #include <QSerialPortInfo>
 #include <QTimer>
+#include <QtConcurrentRun>
 
 #include "devicemanager.h"
 #include "jadeapi.h"
@@ -33,89 +34,95 @@ bool FilterSerialPort(const QSerialPortInfo& info)
 JadeDeviceSerialPortDiscoveryAgent::JadeDeviceSerialPortDiscoveryAgent(QObject* parent)
     : QObject(parent)
 {
-    auto timer = new QTimer(this);
-    connect(timer, &QTimer::timeout, this, &JadeDeviceSerialPortDiscoveryAgent::scan);
-    timer->start(2000);
     scan();
 }
 
 void JadeDeviceSerialPortDiscoveryAgent::scan()
 {
-    auto devices = m_devices;
-    m_devices.clear();
+    using Watcher = QFutureWatcher<QList<QSerialPortInfo>>;
+    const auto watcher = new Watcher(this);
 
-    if (m_reset_countdown == 0) {
-        m_failed_locations.clear();
-        m_reset_countdown = 0;
-    } else {
-        --m_reset_countdown;
-    }
+    watcher->setFuture(QtConcurrent::run([=] {
+        return QSerialPortInfo::availablePorts();
+    }));
 
-    auto failed_locations = m_failed_locations;
-    m_failed_locations.clear();
+    connect(watcher, &Watcher::finished, this, [=] {
+        auto devices = m_devices;
+        m_devices.clear();
 
-    for (const auto &info : QSerialPortInfo::availablePorts()) {
-        const auto system_location = info.systemLocation();
-        if (failed_locations.contains(system_location)) {
-            m_failed_locations.insert(system_location);
-            continue;
+        if (m_reset_countdown == 0) {
+            m_failed_locations.clear();
+            m_reset_countdown = 0;
+        } else {
+            --m_reset_countdown;
         }
 
-        if (FilterSerialPort(info)) continue;
+        auto failed_locations = m_failed_locations;
+        m_failed_locations.clear();
 
-        auto device = devices.take(system_location);
-        if (!device) {
-            bool relax_write = false;
+        for (const auto &info : watcher->result()) {
+            const auto system_location = info.systemLocation();
+            if (failed_locations.contains(system_location)) {
+                m_failed_locations.insert(system_location);
+                continue;
+            }
+
+            if (FilterSerialPort(info)) continue;
+
+            auto device = devices.take(system_location);
+            if (!device) {
+                bool relax_write = false;
 #ifdef Q_OS_MACOS
-            relax_write = system_location.contains("cu.usbmodem");
+                relax_write = system_location.contains("cu.usbmodem");
 #endif
-            auto api = new JadeAPI(info, relax_write);
-            device = new JadeDevice(api, system_location, this);
-            api->setParent(device);
-            connect(api, &JadeAPI::onConnected, this, [this, device] {
-                device->api()->getVersionInfo([=](const QVariantMap& data) {
-                    if (data.contains("error")) {
-                        m_devices.remove(device->systemLocation());
-                        m_failed_locations.insert(device->systemLocation());
-                        device->deleteLater();
-                        return;
-                    }
-                    const auto result = data.value("result").toMap();
-                    device->setVersionInfo(result);
-                    DeviceManager::instance()->addDevice(device);
-                    connect(device, &JadeDevice::error, [=] {
-                        if (m_devices.take(device->systemLocation())) {
-                            DeviceManager::instance()->removeDevice(device);
+                auto api = new JadeAPI(info, relax_write);
+                device = new JadeDevice(api, system_location, this);
+                api->setParent(device);
+                connect(api, &JadeAPI::onConnected, this, [this, device] {
+                    device->api()->getVersionInfo([=](const QVariantMap& data) {
+                        if (data.contains("error")) {
+                            m_devices.remove(device->systemLocation());
+                            m_failed_locations.insert(device->systemLocation());
                             device->deleteLater();
+                            return;
                         }
+                        const auto result = data.value("result").toMap();
+                        device->setVersionInfo(result);
+                        DeviceManager::instance()->addDevice(device);
+                        connect(device, &JadeDevice::error, this, [=] {
+                            if (m_devices.take(device->systemLocation())) {
+                                DeviceManager::instance()->removeDevice(device);
+                                device->deleteLater();
+                            }
+                        });
                     });
                 });
-            });
-            connect(api, &JadeAPI::onOpenError, this, [this, device] {
-                m_failed_locations.insert(device->systemLocation());
-            });
-            connect(api, &JadeAPI::onDisconnected, this, [this, device] {
-                if (m_devices.take(device->systemLocation())) {
-                    DeviceManager::instance()->removeDevice(device);
-                    delete device;
-                }
-            });
-            m_devices.insert(system_location, device);
-            api->connectDevice();
-        } else if (device->api()->isConnected()) {
-            m_devices.insert(system_location, device);
-        } else {
-            devices.insert(system_location, device);
+                connect(api, &JadeAPI::onOpenError, this, [=] {
+                    m_failed_locations.insert(device->systemLocation());
+                });
+                connect(api, &JadeAPI::onDisconnected, this, [=] {
+                    if (m_devices.take(device->systemLocation())) {
+                        DeviceManager::instance()->removeDevice(device);
+                        delete device;
+                    }
+                });
+                m_devices.insert(system_location, device);
+                api->connectDevice();
+            } else if (device->api()->isConnected()) {
+                m_devices.insert(system_location, device);
+            } else {
+                devices.insert(system_location, device);
+            }
         }
-    }
 
-    if (devices.empty()) return;
+        while (!devices.empty()) {
+            const auto system_location = devices.firstKey();
+            auto device = devices.take(system_location);
+            DeviceManager::instance()->removeDevice(device);
+            device->api()->disconnectDevice();
+            delete device;
+        }
 
-    while (!devices.empty()) {
-        const auto system_location = devices.firstKey();
-        auto device = devices.take(system_location);
-        DeviceManager::instance()->removeDevice(device);
-        device->api()->disconnectDevice();
-        delete device;
-    }
+        QTimer::singleShot(2000, this, &JadeDeviceSerialPortDiscoveryAgent::scan);
+    });
 }
