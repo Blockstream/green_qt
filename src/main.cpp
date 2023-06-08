dd7b5f55662aa12071197702042bdfa6179d1558
Borgbyte <borgbyte@gmail.com>
2023-06-08 14:32:22 +0100

    fix: production build has empty log filename

--- src/main.cpp
+++ src/main.cpp
@@ -104,7 +104,10 @@ static QString g_gdk_debug_buffer;
 
 void initLog()
 {
-    g_log_file.setFileName(GetDataFile("logs", QString("%1.txt").arg(GREEN_LOG_FILE)));
+    const QString log_file(GREEN_LOG_FILE);
+    const QString version(GREEN_VERSION);
+
+    g_log_file.setFileName(GetDataFile("logs", QString("%1.txt").arg(log_file.isEmpty() ? version : log_file)));
 
     if (QString{"Development"} != GREEN_ENV) {
         g_log_file.open(QIODevice::WriteOnly | QIODevice::Append);
