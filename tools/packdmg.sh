bd2dd620d6408ac7f6f462c4fd0a038fa8ede38a
Borgbyte <borgbyte@gmail.com>
2023-06-08 09:54:38 +0100

    build: set app name in macos

--- tools/packdmg.sh
+++ tools/packdmg.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 rm -rf staging tmp.dmg Blockstream\ Green.dmg
 cp -R tools/staging staging
-cp -R $1 staging/Blockstream\ Green.app
+cp -R "$1" staging/Blockstream\ Green.app
 ln -s /Applications staging/Applications
 hdiutil makehybrid -hfs -hfs-volume-name "Blockstream Green" -hfs-openfolder staging staging -o tmp.dmg
 hdiutil convert -format UDZO tmp.dmg -o Blockstream\ Green.dmg
