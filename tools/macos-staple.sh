b50b61ee0486f34cbe9d614213d05642ffba3029
Borgbyte <borgbyte@gmail.com>
2023-09-11 18:14:33 +0100

    ci: support debugging notarized macos app

--- tools/macos-staple.sh
+++ tools/macos-staple.sh
@@ -3,10 +3,11 @@ set -eox pipefail
 
 FILE=$1
 TEAM_ID=D9W37S9468
+ENTITLEMENTS=$([[ $CI_COMMIT_REF_NAME = release_* ]] && echo "entitlements.plist" || echo "entitlements_debug.plist")
 
 codesign \
   --options runtime \
-  --entitlements entitlements.plist \
+  --entitlements $ENTITLEMENTS \
   --deep "$FILE" \
   -s "Developer ID Application: Blockstream Corporation ($TEAM_ID)"
 
