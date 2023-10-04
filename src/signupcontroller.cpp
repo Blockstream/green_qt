4e9f2e238979397ccb229a7424304cc4ec88ec66
Borgbyte <borgbyte@gmail.com>
2023-10-04 12:58:24 +0100

    app: clean signup controller

--- src/signupcontroller.cpp
+++ src/signupcontroller.cpp
@@ -28,13 +28,6 @@ void SignupController::setNetwork(Network* network)
     m_dispatcher->dispatch();
 }
 
-void SignupController::setType(const QString& type)
-{
-    if (m_type == type) return;
-    m_type = type;
-    emit typeChanged();
-}
-
 void SignupController::setPin(const QString& pin)
 {
     if (m_pin == pin) return;
@@ -62,11 +55,11 @@ void SignupController::setActive(bool active)
     auto mnemonic_login = new LoginTask(m_mnemonic, QString(), session);
     auto encrypt_with_pin = new EncryptWithPinTask(credentials, m_pin, session);
     auto create_wallet = new SignupCreateWalletTask(this);
-    auto load_twofactor_config = new LoadTwoFactorConfigTask(session);
-    auto load_currencies = new LoadCurrenciesTask(session);
-    auto get_watchonly_details = new GetWatchOnlyDetailsTask(session);
-    auto load_assets = new LoadAssetsTask(session);
-    auto load_accounts = new LoadAccountsTask(false, session);
+//    auto load_twofactor_config = new LoadTwoFactorConfigTask(session);
+//    auto load_currencies = new LoadCurrenciesTask(session);
+//    auto get_watchonly_details = new GetWatchOnlyDetailsTask(session);
+//    auto load_assets = new LoadAssetsTask(session);
+//    auto load_accounts = new LoadAccountsTask(false, session);
     auto persist_wallet = new SignupPersistWalletTask(this);
     auto get_credentials = new GetCredentialsTask(session);
 
@@ -75,11 +68,11 @@ void SignupController::setActive(bool active)
     group->add(mnemonic_login);
     group->add(encrypt_with_pin);
     group->add(create_wallet);
-    group->add(load_twofactor_config);
-    group->add(load_currencies);
-    group->add(get_watchonly_details);
-    group->add(load_assets);
-    group->add(load_accounts);
+//    group->add(load_twofactor_config);
+//    group->add(load_currencies);
+//    group->add(get_watchonly_details);
+//    group->add(load_assets);
+//    group->add(load_accounts);
     group->add(persist_wallet);
     group->add(get_credentials);
 
@@ -87,46 +80,46 @@ void SignupController::setActive(bool active)
     mnemonic_login->then(encrypt_with_pin);
     create_wallet->needs(encrypt_with_pin);
     create_wallet->then(get_credentials);
-    load_twofactor_config->needs(create_wallet);
-    load_currencies->needs(create_wallet);
-    get_watchonly_details->needs(create_wallet);
-    load_assets->needs(create_wallet);
-    load_accounts->needs(create_wallet);
-    persist_wallet->needs(load_accounts);
+//    load_twofactor_config->needs(create_wallet);
+//    load_currencies->needs(create_wallet);
+//    get_watchonly_details->needs(create_wallet);
+//    load_assets->needs(create_wallet);
+//    load_accounts->needs(create_wallet);
+    persist_wallet->needs(get_credentials);
 
-    if (m_type == "amp") {
-        const auto account = QJsonObject{
-            { "name", "AMP Account" },
-            { "type", "2of2_no_recovery" }
-        };
+//    if (m_type == "amp") {
+//        const auto account = QJsonObject{
+//            { "name", "AMP Account" },
+//            { "type", "2of2_no_recovery" }
+//        };
 
-        auto network = m_context->wallet()->network();
-        auto session = m_context->getOrCreateSession(network);
-        auto create_amp_account = new CreateAccountTask(account, session);
-        auto hide_main_account = new UpdateAccountTask(QJsonObject{{ "subaccount", 0 }, { "hidden", true }}, session);
+//        auto network = m_context->wallet()->network();
+//        auto session = m_context->getOrCreateSession(network);
+//        auto create_amp_account = new CreateAccountTask(account, session);
+//        auto hide_main_account = new UpdateAccountTask(QJsonObject{{ "subaccount", 0 }, { "hidden", true }}, session);
 
-        mnemonic_login->then(create_amp_account);
-        create_amp_account->then(hide_main_account);
-        hide_main_account->then(load_accounts);
+//        mnemonic_login->then(create_amp_account);
+//        create_amp_account->then(hide_main_account);
+//        hide_main_account->then(load_accounts);
 
-        group->add(mnemonic_login);
-        group->add(create_amp_account);
-        group->add(hide_main_account);
-    }
+//        group->add(mnemonic_login);
+//        group->add(create_amp_account);
+//        group->add(hide_main_account);
+//    }
 
-    if (m_network->isElectrum()) {
-        const auto account = QJsonObject{
-            { "name", "Segwit Account" },
-            { "type", "p2wpkh" }
-        };
+//    if (m_network->isElectrum()) {
+//        const auto account = QJsonObject{
+//            { "name", "Segwit Account" },
+//            { "type", "p2wpkh" }
+//        };
 
-        auto create_segwit_account = new CreateAccountTask(account, session);
+//        auto create_segwit_account = new CreateAccountTask(account, session);
 
-        mnemonic_login->then(create_segwit_account);
-        create_segwit_account->then(load_accounts);
+//        mnemonic_login->then(create_segwit_account);
+//        create_segwit_account->then(load_accounts);
 
-        group->add(create_segwit_account);
-    }
+//        group->add(create_segwit_account);
+//    }
 
     m_dispatcher->add(group);
 }
@@ -157,11 +150,7 @@ void SignupCreateWalletTask::update()
     const auto wallet = WalletManager::instance()->createWallet(network, wallet_hash_id);
     wallet->m_is_persisted = true;
 
-    if (m_controller->type() == "amp") {
-        wallet->setName(WalletManager::instance()->uniqueWalletName("My AMP Wallet"));
-    } else {
-        wallet->setName(WalletManager::instance()->newWalletName(network));
-    }
+    wallet->setName(WalletManager::instance()->newWalletName(network));
     wallet->m_pin_data = QJsonDocument(context->m_pin_data).toJson();
     wallet->save();
 
@@ -191,5 +180,5 @@ void SignupPersistWalletTask::update()
 
     setStatus(Status::Finished);
 
-    emit m_controller->signup(wallet);
+    emit m_controller->registerFinished(context);
 }
--- src/signupcontroller.h
+++ src/signupcontroller.h
@@ -14,7 +14,6 @@ class SignupController : public Controller
     Q_OBJECT
     Q_PROPERTY(Network* network READ network WRITE setNetwork NOTIFY networkChanged)
     Q_PROPERTY(QStringList mnemonic READ mnemonic WRITE setMnemonic NOTIFY mnemonicChanged)
-    Q_PROPERTY(QString type READ type WRITE setType NOTIFY typeChanged)
     Q_PROPERTY(QString pin READ pin WRITE setPin NOTIFY pinChanged)
     Q_PROPERTY(bool active READ active WRITE setActive NOTIFY activeChanged)
     Q_PROPERTY(Wallet* wallet READ wallet NOTIFY walletChanged)
@@ -27,9 +26,6 @@ public:
 
     QStringList mnemonic() const { return m_mnemonic; }
     void setMnemonic(const QStringList &mnemonic);
-
-    QString type() const { return m_type; }
-    void setType(const QString& type);
 
     QString pin() const { return m_pin; }
     void setPin(const QString& pin);
@@ -45,16 +41,14 @@ signals:
     void walletChanged();
     void pinChanged();
     void activeChanged();
-    void typeChanged();
     void mnemonicSizeChanged();
     void mnemonicChanged();
-    void signup(Wallet* wallet);
+    void registerFinished(Context* context);
 
 private:
     Network* m_network{nullptr};
     QStringList m_mnemonic;
     QString m_pin;
-    QString m_type{"default"};
     bool m_active{false};
     Wallet* m_wallet{nullptr};
 };
