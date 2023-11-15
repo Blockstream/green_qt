cd2efd0c1901873c8abb310a0d38c034d5f8b16f
Borgbyte <borgbyte@gmail.com>
2023-11-15 10:08:00 +0000

    gdk: update to 0.69.0

--- tools/buildgdk.sh
+++ tools/buildgdk.sh
@@ -6,7 +6,7 @@ mkdir -p build
 cd build
 
 if [ ! -d gdk ]; then
-    git clone --quiet --depth 1 --branch release_0.68.4 --single-branch https://github.com/Blockstream/gdk.git gdk
+    git clone --quiet --depth 1 --branch release_0.69.0 --single-branch https://github.com/Blockstream/gdk.git gdk
 fi
 
 cd gdk
