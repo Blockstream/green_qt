001a37b09a5c632093315f97fddae47ca169c2b2
Borgbyte <borgbyte@gmail.com>
2024-01-23 10:37:54 +0000

    ci: publish multi-arch

--- tools/publish.sh
+++ tools/publish.sh
@@ -3,11 +3,6 @@ set -eo pipefail
 
 VERSION=2.0.0
 
-HASH_WINDOWS=`cat SHA256SUMS.asc | grep Windows | awk '{split($0, a," "); print a[1]}'`
-HASH_MAC=`cat SHA256SUMS.asc | grep Mac | awk '{split($0, a," "); print a[1]}'`
-HASH_LINUX=`cat SHA256SUMS.asc | grep AppImage | awk '{split($0, a," "); print a[1]}'`
-
-
 if [[ -z "$CHANNEL" ]]; then
     echo "Must set CHANNEL environment variable" 1>&2
     echo "For public release set CHANNEL=latest" 1>&2
@@ -16,18 +11,19 @@ fi
 
 cp ../tools/templates/channel.json ${CHANNEL}.json
 
+sed -i -e "s/CHANNEL_STRING/${CHANNEL}/g" ${CHANNEL}.json
 sed -i -e "s/VERSION_STRING/${VERSION}/g" ${CHANNEL}.json
-sed -i -e "s/HASH_WINDOWS/${HASH_WINDOWS}/g" ${CHANNEL}.json
-sed -i -e "s/HASH_MAC/${HASH_MAC}/g" ${CHANNEL}.json
-sed -i -e "s/HASH_LINUX/${HASH_LINUX}/g" ${CHANNEL}.json
 
 echo $GCLOUD_KEY > .key
 gcloud auth activate-service-account --key-file=.key
 gsutil -h "Cache-Control: no-store" cp ${CHANNEL}.json gs://${GCLOUD_BUCKET}/desktop/
 
-gsutil cp SHA256SUMS.asc gs://${GCLOUD_BUCKET}/desktop/${VERSION}/
-gsutil cp BlockstreamGreen_Windows_x86_64.zip gs://${GCLOUD_BUCKET}/desktop/${VERSION}/
-gsutil cp BlockstreamGreen_MacOS_x86_64.zip gs://${GCLOUD_BUCKET}/desktop/${VERSION}/
-gsutil cp BlockstreamGreen-x86_64.AppImage gs://${GCLOUD_BUCKET}/desktop/${VERSION}/
+gsutil cp SHA256SUMS.asc gs://${GCLOUD_BUCKET}/desktop/${CHANNEL}/${VERSION}/
+gsutil cp BlockstreamGreen-x86_64.AppImage gs://${GCLOUD_BUCKET}/desktop/${CHANNEL}/${VERSION}/
+gsutil cp BlockstreamGreen-linux-x86_64.tar.gz gs://${GCLOUD_BUCKET}/desktop/${CHANNEL}/${VERSION}/
+gsutil cp BlockstreamGreen-universal.dmg gs://${GCLOUD_BUCKET}/desktop/${CHANNEL}/${VERSION}/
+gsutil cp BlockstreamGreen-arm64.dmg gs://${GCLOUD_BUCKET}/desktop/${CHANNEL}/${VERSION}/
+gsutil cp BlockstreamGreen-x86_64.dmg gs://${GCLOUD_BUCKET}/desktop/${CHANNEL}/${VERSION}/
+gsutil cp BlockstreamGreenSetup-x86_64.exe gs://${GCLOUD_BUCKET}/desktop/${CHANNEL}/${VERSION}/
 
 rm .key
