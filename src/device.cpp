48c11f1bb859dfe2e8427efcd47cb8462d2f8e07
Borgbyte <borgbyte@gmail.com>
2024-03-26 17:05:37 +0000

    core: remove per device xpub caching

--- src/device.cpp
+++ src/device.cpp
@@ -18,53 +18,20 @@ QByteArray pathToData(const QVector<uint32_t>& path)
 
 class GetWalletPublickKeyDispatcher
 {
-    QMap<QByteArray, QMap<QVector<uint32_t>, QByteArray>> m_cache;
     QQueue<GetWalletPublicKeyActivity*> m_queue;
     GetWalletPublicKeyActivity* m_activity{nullptr};
-
 public:
     void enqueue(GetWalletPublicKeyActivity* activity)
     {
         m_queue.enqueue(activity);
         next();
     }
-
     void next()
     {
         if (m_activity) return;
         if (m_queue.isEmpty()) return;
-
         m_activity = m_queue.dequeue();
-        auto device = m_activity->device();
-        auto master_public_key = device->masterPublicKey(m_activity->network());
-        auto path = m_activity->path();
-
-        if (!master_public_key.isEmpty()) {
-            if (m_cache.contains(master_public_key)) {
-                const auto& public_key = m_cache[master_public_key][path];
-                if (!public_key.isEmpty()) {
-                    m_activity->setPublicKey(public_key);
-                    m_activity->finish();
-                    m_activity = nullptr;
-                    next();
-                    return;
-                }
-            }
-        }
-
         QObject::connect(m_activity, &Activity::finished, [=]{
-            auto device = m_activity->device();
-            auto master_public_key = device->masterPublicKey(m_activity->network());
-            auto path = m_activity->path();
-            auto publick_key = m_activity->publicKey();
-
-            if (master_public_key.isEmpty() && path.isEmpty()) {
-                master_public_key = publick_key;
-                device->setMasterPublicKey(m_activity->network(), publick_key);
-            }
-            if (!master_public_key.isEmpty()) {
-                m_cache[master_public_key][path] = publick_key;
-            }
             m_activity = nullptr;
             next();
         });
