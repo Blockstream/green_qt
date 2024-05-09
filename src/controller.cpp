53ad5f35bf0b736368079cd420655e96b843fa27
Borgbyte <borgbyte@gmail.com>
2024-05-09 22:26:49 +0100

    core: add bcur controller

--- src/controller.cpp
+++ src/controller.cpp
@@ -4,6 +4,7 @@
 #include "account.h"
 #include "address.h"
 #include "context.h"
+#include "networkmanager.h"
 #include "notification.h"
 #include "task.h"
 #include "session.h"
@@ -538,4 +539,85 @@ void WatchOnlyController::update(const QString& username, const QString& passwor
 void WatchOnlyController::clear()
 {
     update("", "");
+}
+
+BCURController::BCURController(QObject* parent)
+    : Controller(parent)
+{
+}
+
+void BCURController::setProgress(int progress)
+{
+    if (m_progress == progress) return;
+    m_progress = progress;
+    emit progressChanged();
+}
+
+void BCURController::process(const QString& data)
+{
+    if (!m_result.isEmpty()) return;
+
+    if (m_seen.contains(data)) return;
+    m_seen.insert(data);
+
+    if (!data.startsWith("ur:")) {
+        emit dataDiscarded(data);
+        if (m_task) {
+            m_task->deleteLater();
+            m_task = nullptr;
+        }
+        return;
+    }
+
+    if (!m_context) {
+        setContext(new Context("mainnet", false, this));
+    }
+
+    if (!m_task) {
+        auto session = m_context->primarySession();
+        dispatcher()->add(new ConnectTask(session));
+
+        m_task = new DecodeBCURTask(data, session);
+        connect(m_task, &DecodeBCURTask::promptChanged, this, [=] {
+            auto prompt = qobject_cast<CodePrompt*>(m_task->prompt());
+            if (!prompt) return;
+
+            connect(prompt, &CodePrompt::resultChanged, this, [=] {
+                setProgress(prompt->result().value("auth_data").toObject().value("estimated_progress").toInt());
+                next();
+            });
+            next();
+        });
+        connect(m_task, &DecodeBCURTask::resultChanged, this, [=] {
+            next();
+        });
+        connect(m_task, &DecodeBCURTask::finished, this, [=] {
+            m_result = m_task->decodedResult();
+            emit resultDecoded(m_result);
+            m_task->deleteLater();
+            m_task = nullptr;
+        });
+        connect(m_task, &DecodeBCURTask::failed, this, [=] {
+            emit dataDiscarded(data);
+            m_task->deleteLater();
+            m_task = nullptr;
+        });
+        dispatcher()->add(m_task);
+    } else {
+        m_pending.enqueue(data);
+        next();
+    }
+}
+
+void BCURController::next()
+{
+    if (!m_task) return;
+
+    auto prompt = qobject_cast<CodePrompt*>(m_task->prompt());
+    if (!prompt) return;
+
+    if (m_pending.isEmpty()) return;
+
+    const auto part = m_pending.dequeue();
+    prompt->resolve(part);
 }
--- src/controller.h
+++ src/controller.h
@@ -167,4 +167,31 @@ private:
     QString m_signature;
 };
 
+
+class DecodeBCURTask;
+class BCURController : public Controller
+{
+    Q_OBJECT
+    Q_PROPERTY(int progress READ progress NOTIFY progressChanged FINAL)
+    QML_ELEMENT
+public:
+    BCURController(QObject* parent = nullptr);
+    int progress() const { return m_progress; }
+    void setProgress(int progress);
+public slots:
+    void process(const QString& data);
+signals:
+    void progressChanged();
+    void resultDecoded(const QJsonObject& result);
+    void dataDiscarded(const QString& data);
+private:
+    void next();
+private:
+    QJsonObject m_result;
+    QSet<QString> m_seen;
+    QQueue<QString> m_pending;
+    DecodeBCURTask* m_task{nullptr};
+    int m_progress{0};
+};
+
 #endif // GREEN_CONTROLLER_H
