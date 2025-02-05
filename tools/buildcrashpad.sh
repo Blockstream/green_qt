ac82535cdb7bf8aae690eb4180e5b744cc0749bc
qblk <qblk@users.noreply.github.com>
2025-02-05 22:09:39 +0000

    deps: build uasm

--- tools/buildcrashpad.sh
+++ tools/buildcrashpad.sh
@@ -5,29 +5,6 @@ mkdir -p build
 
 cd build
 
-if [[ "$HOST" == "macos" ]]; then
-wget https://www.terraspace.co.uk/uasm247_osx.zip
-unzip uasm247_osx.zip
-chmod +x uasm
-export PATH=$(pwd):$PATH
-fi
-
-if [[ "$HOST" == "linux" ]]; then
-wget https://github.com/Terraspace/UASM/archive/refs/tags/v2.57r.tar.gz
-tar zxvf v2.57r.tar.gz
-cd UASM-2.57r
-make -f Makefile-Linux-GCC-64.mak
-export PATH=$(pwd)/GccUnixR:$PATH
-cd ..
-fi
-
-if [[ "$HOST" == "windows" ]]; then
-wget https://www.terraspace.co.uk/uasm257_linux64.zip
-unzip uasm257_linux64.zip
-chmod +x uasm
-export PATH=$(pwd):$PATH
-fi
-
 CRASHPAD_REPO=https://github.com/Blockstream/crashpad.git
 CRASHPAD_BRANCH=green_qt
 CRASHPAD_COMMIT=1a4c92aca24ca52ffb4e3b798d4a3681f37116d4
