bb49df85883b7ed867fccb4dde5b95c756bbcae9
qblk <qblk@users.noreply.github.com>
2026-04-30 23:34:47 +0100

    ci: update libusb to v1.0.29

--- tools/buildlibusb.sh
+++ tools/buildlibusb.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -eo pipefail
 
-mkdir -p build
+VERSION=1.0.29
+DIRNAME=libusb-${VERSION}
+ARCHIVE=${DIRNAME}.tar.gz
+HASH=7c2dd39c0b2589236e48c93247c986ae272e27570942b4163cb00a060fcf1b74
 
-cd build
+mkdir -p build && cd build
 
-if [ ! -d libusb ]; then
-    git clone --quiet --depth 1 --branch v1.0.27 --single-branch https://github.com/libusb/libusb.git libusb
+if [ ! -d $DIRNAME ]; then
+    curl -s -L -o $ARCHIVE https://github.com/libusb/libusb/archive/refs/tags/v${VERSION}.tar.gz
+    echo "${HASH}  ${ARCHIVE}" | ${SHA256SUM:-sha256sum} --check
+    tar zxf $ARCHIVE
 fi
 
-cd libusb
+cd $DIRNAME
 
 ./bootstrap.sh
 if [ "$HOST" = "linux" ]; then
