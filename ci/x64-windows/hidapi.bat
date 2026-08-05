d079db11b8ca24e4ab9782fb8dffcd64c7bea8a7
qblk <qblk@users.noreply.github.com>
2026-08-05 23:14:39 +0100

    Check hidapi archive hash

--- ci/x64-windows/hidapi.bat
+++ ci/x64-windows/hidapi.bat
@@ -3,8 +3,15 @@ setlocal enabledelayedexpansion
 
 set FILENAME=hidapi-0.15.0
 set ARCHIVE=%FILENAME%.tar.gz
+set HASH=5d84dec684c27b97b921d2f3b73218cb773cf4ea915caee317ac8fc73cef8136
 
 curl -s -L -o %ARCHIVE% https://github.com/libusb/hidapi/archive/refs/tags/%ARCHIVE%
+
+certutil -hashfile %ARCHIVE% SHA256 | findstr /i /c:"%HASH%" >nul
+if errorlevel 1 (
+    echo Checksum verification failed for %ARCHIVE%
+    exit /b 1
+)
 
 7z x %ARCHIVE% -so | 7z x -si -ttar
 
