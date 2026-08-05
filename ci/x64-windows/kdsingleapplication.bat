5994fa9e7db41a8882624c81ca819dba566acc50
qblk <qblk@users.noreply.github.com>
2026-08-05 23:13:21 +0100

    Check kdsingleapplication archive hash

--- ci/x64-windows/kdsingleapplication.bat
+++ ci/x64-windows/kdsingleapplication.bat
@@ -4,8 +4,15 @@ set VERSION=1.2.1
 set FILENAME=kdsingleapplication-%VERSION%
 set ARCHIVE=%FILENAME%.tar.gz
 set DIRNAME=%FILENAME%
+set HASH=e3254ce9dc5ecf6d61ef83264bc61d486a307f0e3c9ed1bb2176f068cdbcbe09
 
 curl -s -L -o %ARCHIVE% https://github.com/KDAB/KDSingleApplication/releases/download/v%VERSION%/%ARCHIVE%
+
+certutil -hashfile %ARCHIVE% SHA256 | findstr /i /c:"%HASH%" >nul
+if errorlevel 1 (
+    echo Checksum verification failed for %ARCHIVE%
+    exit /b 1
+)
 
 7z x %ARCHIVE% -so | 7z x -si -ttar
 
--- tools/buildkdsingleapplication.sh
+++ tools/buildkdsingleapplication.sh
@@ -5,11 +5,13 @@ VERSION=1.2.1
 FILENAME=kdsingleapplication-$VERSION
 ARCHIVE=$FILENAME.tar.gz
 DIRNAME=KDSingleApplication-$VERSION
+HASH=e3254ce9dc5ecf6d61ef83264bc61d486a307f0e3c9ed1bb2176f068cdbcbe09
 
 mkdir -p build && cd build
 
 if [ ! -d $DIRNAME ]; then
     curl -s -L -o $ARCHIVE https://github.com/KDAB/KDSingleApplication/releases/download/v$VERSION/$ARCHIVE
+    echo "${HASH}  ${ARCHIVE}" | ${SHA256SUM:-sha256sum} --check
     tar zxf $ARCHIVE
 fi
 
