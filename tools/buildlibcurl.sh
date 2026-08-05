2a6d62c86b2b9ede871ff0c80fae96e1b29db5fc
qblk <qblk@users.noreply.github.com>
2026-08-05 23:39:22 +0100

    Checkout pinned libcurl commit

--- tools/buildlibcurl.sh
+++ tools/buildlibcurl.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -eo pipefail
 
+COMMIT=400fffa90f30c7a2dc762fa33009d24851bd2016 # curl-8_17_0
+
 mkdir -p build && cd build
 
 git clone https://github.com/curl/curl.git curl-src
 
-(cd curl-src && git checkout curl-8_17_0 && git rev-parse HEAD)
+(cd curl-src && git checkout $COMMIT && git rev-parse HEAD)
 
 cmake -S curl-src -B curl-bld \
   -DCMAKE_BUILD_TYPE=Release \
