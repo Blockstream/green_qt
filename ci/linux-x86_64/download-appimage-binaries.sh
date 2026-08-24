395dfd4d93738625e3b2eeec8294e20bc28f6a86
qblk <qblk@users.noreply.github.com>
2026-08-24 15:20:37 +0100

    Update linuxdeploy-plugin-qt AppImage checksum

--- ci/linux-x86_64/download-appimage-binaries.sh
+++ ci/linux-x86_64/download-appimage-binaries.sh
@@ -6,7 +6,7 @@ echo "421ca71d5c69ea97c6309276232990d43df1dcece0edfaa26bbf926ff96ed12e  linuxdep
 chmod +x linuxdeploy-x86_64.AppImage
 
 curl -sL -o linuxdeploy-plugin-qt-x86_64.AppImage https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
-echo "be1b7e166bf9975cfb694ebe6759ba40502ffc6196440d3e64aa90c4dbd67e9f  linuxdeploy-plugin-qt-x86_64.AppImage" | sha256sum --check
+echo "cfc1055b2b9dbc08412b579f20990b7b41a17b61beaa5847dc9477c96c9e9617  linuxdeploy-plugin-qt-x86_64.AppImage" | sha256sum --check
 chmod +x linuxdeploy-plugin-qt-x86_64.AppImage
 
 curl -sL -o appimagetool-x86_64.AppImage https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
